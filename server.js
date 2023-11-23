170e2b03029e357efb2fd827669f4a7eb4519ad8
Sowwiezn <businka16.04.2007@gmail.com>
2023-11-23 17:57:08 +0200

    v3

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const pathToIndex = path.join(__dirname, 'hg', 'index.html')
 const indexHtmlFile = fs.readFileSync(pathToIndex)
 const pathJs = path.join(__dirname, 'hg', 'js.js')
 const fileJs = fs.readFileSync(pathJs)
-const pathNode = path.join(__dirname, 'hg', 'style.css')
-const fileNode = fs.readFileSync(pathNode)
+const pathCss = path.join(__dirname, 'hg', 'style.css')
+const fileCss = fs.readFileSync(pathCss)
 
 const server = http.createServer((req, res) =>{
     switch(req.url){
         case '/': return res.end(indexHtmlFile)
         case '/js.js': return res.end(fileJs)
-        case '/stule.css': return res.end(fileNode)
+        case '/stule.css': return res.end(fileCss)
     }
     res.statusCode = 404
     return res.end('Errore 404')
